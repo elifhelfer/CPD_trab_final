89147eb5322e777d30e5fcadb7339343e67673d2
lfurtadoh <larissa.furtado@ufrgs.br>
2024-08-04 02:28:54 -0300

    trie cm insercao e pesquisa

--- trie.py
+++ trie.py
@@ -1,10 +1,10 @@
-class trie_node():
+class trie_node:
     def __init__(self, letter: str):
         self.letter = letter
         self.children = []
         self.is_end = False
 
-class trie_tree():
+class trie_tree:
     def __init__(self):
         self.root = trie_node(None)
 
@@ -16,37 +16,45 @@ class trie_tree():
                 if child.letter == word[i]:
                     aux_node = child
                     found = True
-                    if i >= len(word) - 1:
+                    if i == len(word)-1:
                         child.is_end = True         #trocar para ID sofifa
             if found == False:
                 new_node = trie_node(word[i])
                 aux_node.children.append(new_node)
                 aux_node = new_node
+                if i == len(word)-1:
+                        aux_node.is_end = True         #trocar para ID sofifa
                 
-                               
+    def collect_words(self, node: trie_node, prefix: str, words: list):
+        if node.is_end:
+            words.append(prefix)
+        for child in node.children:
+            self.collect_words(child, prefix+child.letter, words)
+        return words
+
+
     def search(self, word: str):
         aux_node = self.root
-        results = []
-        partial_result = ''
-        found = False
+        words = []
         for letter in word:
+            found = False
             for child in aux_node.children:
                 if child.letter == letter:
                     aux_node = child
                     found = True
+                    break
         if not found:
             return None
-        results = collect_words(child, word)
+        results = self.collect_words(aux_node, word, words)
+        return results
             
-        def collect_words(node: trie_node, prefix: str):
-            
+if __name__ == "__main__":
 
+    tree = trie_tree()
+    tree.insert('tese')
+    tree.insert('teste')
+    tree.insert('testes')
+    tree.insert('samba')
+    tree.insert('sans')
 
-tree = trie_tree()
-tree.insert('tese')
-tree.insert('teste')
-tree.insert('testes')
-tree.insert('samba')
-tree.insert('sans')
-
-print(tree.search('tr'))
+    print(tree.search('tes'))
