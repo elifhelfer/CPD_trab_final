7e51530215d3f2c59234090fa6a174f750ae826f
lfurtadoh <larissa.furtado@ufrgs.br>
2024-08-04 17:20:14 -0300

    trie now returns names of players and their sofifa id's

--- trie.py
+++ trie.py
@@ -1,3 +1,5 @@
+import csv
+
 class trie_node:
     def __init__(self, letter: str):
         self.letter = letter
@@ -8,7 +10,7 @@ class trie_tree:
     def __init__(self):
         self.root = trie_node(None)
 
-    def insert(self, word: str):
+    def insert(self, word: str, id: int):
         aux_node = self.root
         for i in range(len(word)):
             found = False
@@ -21,11 +23,11 @@ class trie_tree:
                 aux_node.children.append(new_node)
                 aux_node = new_node
             if i == len(word)-1:
-                        aux_node.is_end = True         #trocar para ID sofifa
+                        aux_node.is_end = id
                 
     def collect_words(self, node: trie_node, prefix: str, words: list):
         if node.is_end:
-            words.append(prefix)
+            words.append((prefix, node.is_end))
         for child in node.children:
             self.collect_words(child, prefix+child.letter, words)
         return words
@@ -47,12 +49,14 @@ class trie_tree:
         return results
             
 if __name__ == "__main__":
-
+    
+    csv_file = './arquivos-parte1/players.csv'
     tree = trie_tree()
-    tree.insert('tese')
-    tree.insert('teste')
-    tree.insert('testes')
-    tree.insert('samba')
-    tree.insert('sans')
 
-    print(tree.search('d'))
+    with open(csv_file, mode='r', newline='') as file:
+        csv_reader = csv.reader(file)
+        
+        for row in csv_reader:
+            tree.insert(row[2], row[0])
+    
+    print(tree.search('Paul'))
