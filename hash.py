b5651d79eaa90794eca098fcb59d044f98ec54fd
Guifernandes0521 <guifernandes0521@gmail.com>
2024-08-04 22:59:59 -0300

    tempo de montagem jogador + user 132s

--- hash.py
+++ hash.py
@@ -24,26 +24,63 @@ class FifaHash:
 class PlayerHash(FifaHash):
     def __init__(self, hash_size):
         super().__init__(hash_size)
-        pass
-    pass
+    
+    def insert_review(self, key, score):
+        index = self.hash_function(key)
+        
+        for element in self.buckets[index]:
+            if int(element[0]) == key:
+                element[-1] += score
+                element[-2] += 1
+                return 
+        print("Player reviewd not found")
+        return
 
 class UserHash(FifaHash):
     def __init__(self, hash_size):
         super().__init__(hash_size)
-        pass
-    pass
+
+    def insert(self, key, info):
+        index = self.hash_function(key)
+        # if the element was already inserted in bucket
+        for element in self.buckets[index]:
+            if int(element[0]) == key:
+                element.append(info)
+                return 
+        # if it hasn't been inserted yet
+        self.buckets[index].append([key, info])
+        return
 
 if __name__ == "__main__":
     import csv
+    import time
     
-    player_hash = FifaHash(1999)
-
-    with open('/home/guifernandes0521/final_cpd/CPD_trab_final/players.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
+    start_time = time.time()
+    player_hash = PlayerHash(4999)
+    with open('/home/guifernandes0521/final_cpd/CPD_trab_final/players.csv', newline='') as fifa_players:
+        reader = csv.reader(fifa_players)
         # skips header
         next(reader)
 
         for row in reader:
-            key = int(row[0])
-            info = row[0:]
-            player_hash.insert(key, info)      
+            # last two fields: [amount of reviews, score of review]
+            player_hash.insert(int(row[0]), row + [0,0])      
+
+    user_hash = UserHash(59999)
+    
+    with open('/home/guifernandes0521/final_cpd/CPD_trab_final/rating.csv', newline='') as user_reviews:
+        reader = csv.reader(user_reviews)
+        # skips header
+        next(reader)
+        
+        for row in reader:
+            # (player_id, score)
+            player_hash.insert_review(int(row[1]), float(row[2]))
+            # (user_id, info)
+            user_hash.insert(int(row[0]), (row[1], row[2]))
+    
+    end_time = time.time()
+    
+    # Calcular e imprimir o tempo total de execução
+    execution_time = end_time - start_time
+    print(f"Tempo de execução do segundo bloco de leitura de arquivo: {execution_time:.2f} segundos")
